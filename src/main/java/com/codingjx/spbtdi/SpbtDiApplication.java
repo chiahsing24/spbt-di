3d1967c7dbc244520574cc385210d0219db60d2b
chiahsing24 <chiahsingtony@gmail.com>
2022-11-25 20:30:14 -0800

    Spring context usage

--- src/main/java/com/codingjx/spbtdi/SpbtDiApplication.java
+++ src/main/java/com/codingjx/spbtdi/SpbtDiApplication.java
@@ -1,13 +1,22 @@
 package com.codingjx.spbtdi;
 
+import com.codingjx.spbtdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpbtDiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpbtDiApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SpbtDiApplication.class, args);
+
+        // Spring framework helps creating the instance of MyController.
+        MyController myController = (MyController) ctx.getBean("myController");
+
+        String greeting = myController.sayHello();
+
+        System.out.println(greeting);
     }
 
 }
