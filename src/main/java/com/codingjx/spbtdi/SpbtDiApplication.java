87681ea9156cccc97c92413b2255fd7def5ddbed
chiahsing24 <chiahsingtony@gmail.com>
2022-11-25 20:20:01 -0800

    springboot dependency injection project setup

---
+++ src/main/java/com/codingjx/spbtdi/SpbtDiApplication.java
@@ -0,0 +1,13 @@
+package com.codingjx.spbtdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbtDiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingjx/spbtdi/SpbtDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingjx.spbtdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbtDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
